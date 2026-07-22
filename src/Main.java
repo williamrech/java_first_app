4b00283d116518755315b8d3ff3d2d8142db60dc
williamrech <wprech@gmail.com>
2026-07-22 13:34:27 -0300

    FEAT: Lesson 1 - First Java program

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        // THIS IS MY FIRST JAVA PROJECT
+
+        /*
+        Multi-line
+        comment.
+        */
+
+        System.out.println("Every breath you take");
+        System.out.println("Every move you make");
+        System.out.print("I`ll be watching you");
+    }
+}
